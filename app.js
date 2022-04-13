89a99c6077b4f5dfcf170bb653c016dd23cde2dc
MarkLannen <marklannen@gmail.com>
2022-04-12 21:14:59 -0600

    finish Deck of cards with shuffle function

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ function makeDeck() {}
 
 const cardDeck = {
   deck: [],
+  drawnCards: [],
   suits: ['hearts', 'diamonds', 'spades', 'clubs'],
   values: '2,3,4,5,6,7,8,9,10,J,Q,K,A',
   initializeDeck() {
@@ -16,6 +17,22 @@ const cardDeck = {
     }
   },
   drawCard() {
-    return this.deck.pop();
+    const card = this.deck.pop();
+    this.drawnCards.push(card);
+    return card;
+  },
+  drawMultiple(numCards) {
+    const cards = [];
+    for (let i = 0; i < numCards; i++) {
+      cards.push(this.drawCard());
+    }
+    return cards;
+  },
+  shuffle() {
+    const { deck } = this;
+    for (let i = deck.length - 1; i > 0; i--) {
+      let j = Math.floor(Math.random() * (i + 1));
+      [deck[i], deck[j]] = [deck[j], deck[i]];
+    }
   },
 };
